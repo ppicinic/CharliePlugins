8336a07a008d96351587e5b39bec716ab0449f13
Gregory Cremins <coolwhar@gmail.com>
2014-03-12 19:01:06 -0400

    Added split bot plays

--- src/charlie/advisor/StrategyCard.java
+++ src/charlie/advisor/StrategyCard.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package charlie.advisor;
 
 import charlie.card.Card;
@@ -14,64 +13,60 @@ import java.util.HashMap;
 
 /**
  *
- * @author Phil Picinic
- * This is a singleton class that creates a single instance of the Black Jack
- * Strategy Card.
+ * @author Phil Picinic This is a singleton class that creates a single instance
+ * of the Black Jack Strategy Card.
  */
 public class StrategyCard {
- 
+
     // hashmap of arraylists to represent the card
     private final HashMap<String, ArrayList<Play>> card;
-    
-    private static volatile StrategyCard instance = null;   
-    
+
+    private static volatile StrategyCard instance = null;
+
     /**
-     * Constructor
-     * creates and fills in the card
+     * Constructor creates and fills in the card
      */
-    private StrategyCard(){
+    private StrategyCard() {
         card = new HashMap<>();
         ArrayList<Play> temp;
-        
-        for(int i = 9; i < 17; i++){
+
+        for (int i = 9; i < 17; i++) {
             String hand = "" + i;
             card.put(hand, new ArrayList<Play>(11));
             temp = card.get(hand);
             fill(temp, Play.HIT);
-            if(i >= 12 && i <= 16){
-                
-                for(int y = 2; y < 12; y++){
-                    
-                    if(y <= 6){
+            if (i >= 12 && i <= 16) {
+
+                for (int y = 2; y < 12; y++) {
+
+                    if (y <= 6) {
                         temp.set(y, Play.STAY);
                     }
                 }
-                if(i == 12){
+                if (i == 12) {
                     temp.set(2, Play.HIT);
                     temp.set(2, Play.HIT);
                 }
-                
-            }
-            else if(i == 11 || i == 10){
-                for(int y = 2; y < 10; y++){
+
+            } else if (i == 11 || i == 10) {
+                for (int y = 2; y < 10; y++) {
                     temp.set(y, Play.DOUBLE_DOWN);
                 }
-                if(i == 11){
+                if (i == 11) {
                     temp.set(10, Play.DOUBLE_DOWN);
                     temp.set(11, Play.HIT);
-                }else{
+                } else {
                     temp.set(10, Play.HIT);
                     temp.set(11, Play.HIT);
                 }
-            }
-            else{
-                for(int y = 2; y < 12; y++){
-                    if(y >= 3 && y <=6){
+            } else {
+                for (int y = 2; y < 12; y++) {
+                    if (y >= 3 && y <= 6) {
                         temp.set(y, Play.DOUBLE_DOWN);
                     }
                 }
             }
-            
+
         }
         card.put("17+", new ArrayList<Play>(11));
         temp = card.get("17+");
@@ -84,11 +79,10 @@ public class StrategyCard {
         card.put("A, 7", new ArrayList<Play>(11));
         temp = card.get("A, 7");
         fill(temp, Play.DOUBLE_DOWN);
-        for(int y = 2; y < 12; y++){
-            if(y == 2 || y == 7 || y == 8 ){
+        for (int y = 2; y < 12; y++) {
+            if (y == 2 || y == 7 || y == 8) {
                 temp.set(y, Play.STAY);
-            }
-            else if(y >= 9){
+            } else if (y >= 9) {
                 temp.set(y, Play.HIT);
             }
         }
@@ -105,7 +99,7 @@ public class StrategyCard {
         temp.set(4, Play.HIT);
         card.put("A, 3", temp);
         card.put("A, 2", temp);
-        
+
         card.put("A,A 8,8", new ArrayList<Play>());
         temp = card.get("A,A 8,8");
         fill(temp, Play.SPLIT);
@@ -115,126 +109,170 @@ public class StrategyCard {
         temp.set(11, Play.STAY);
         card.put("9, 9", temp);
         temp = (ArrayList<Play>) card.get("A,A 8,8").clone();
-        for(int y = 8; y < 12; y++){
+        for (int y = 8; y < 12; y++) {
             temp.set(y, Play.HIT);
         }
         card.put("7, 7", temp);
         card.put("3, 3", temp);
         card.put("2, 2", temp);
-        
+
         temp = (ArrayList<Play>) temp.clone();
         temp.set(7, Play.HIT);
         card.put("6, 6", temp);
         temp = (ArrayList<Play>) temp.clone();
-        for(int y = 2; y <= 4; y++){
+        for (int y = 2; y <= 4; y++) {
             temp.set(y, Play.HIT);
         }
         card.put("4, 4", temp);
         temp = card.get("10");
         card.put("5, 5", temp);
-        
+
     }
-    
+
     /**
-     * gets an instance of the strategy card
-     * if there isn't one, one will be created
+     * gets an instance of the strategy card if there isn't one, one will be
+     * created
+     *
      * @return the strategy card
      */
-    public static StrategyCard getStrategyCard(){
+    public static StrategyCard getStrategyCard() {
         if (instance == null) {
-            synchronized(StrategyCard.class){
-                if (instance == null){
+            synchronized (StrategyCard.class) {
+                if (instance == null) {
                     instance = new StrategyCard();
                 }
             }
         }
-        
+
         return instance;
     }
-    
+
     /**
      * fills an array list with a decided move
+     *
      * @param array the array to fill
      */
-    private void fill(ArrayList<Play> array, Play play){
-        for(int i = 0; i < 12; i++){
+    private void fill(ArrayList<Play> array, Play play) {
+        for (int i = 0; i < 12; i++) {
             array.add(i, play);
         }
     }
-    
+
     /**
      * gets the play suggested by the card
+     *
      * @param myHand the player's hand
      * @param upCard the dealer's up card
      * @return the play that is suggested
      */
-    public Play getPlay(Hand myHand, Card upCard){
+    public Play getPlay(Hand myHand, Card upCard) {
         String hand = createHandString(myHand);
         int cardVal = upCard.value();
-        if(upCard.isFace()){
+        if (upCard.isFace()) {
             cardVal = 10;
         }
-        if(upCard.isAce()){
+        if (upCard.isAce()) {
             cardVal = 11;
         }
         ArrayList<Play> temp = card.get(hand);
         return temp.get(cardVal);
     }
-    
+
+    /**
+     * Method to get hard play value of hand
+     *
+     * @param myHand the player's hand
+     * @param upCard the dealer's up card
+     * @return the play that the basic strategy suggests
+     *
+     */
+    public Play getBotPlay(Hand myHand, Card upCard) {
+        Play play = getPlay(myHand, upCard);
+        if (play == Play.SPLIT) {
+            Card myCard = myHand.getCard(0);
+            int handVal = myCard.value() * 2;
+            if (myCard.isAce()) {
+                handVal = 12;
+            }
+            if (myCard.isFace()) {
+                handVal = 20;
+            }
+            String handString = "" + handVal;
+            if (handVal >= 17) {
+                handString = "17+";
+            }
+            if (handVal <= 8) {
+                handString = "5-8";
+            }
+            int cardVal = upCard.value();
+            if (upCard.isFace()) {
+                cardVal = 10;
+            }
+            if (upCard.isAce()) {
+                cardVal = 11;
+            }
+            ArrayList<Play> temp = card.get(handString);
+            play = temp.get(cardVal);
+        }
+
+        return play;
+
+    }
+
     /**
      * creates a hand string code based on the hand
+     *
      * @param hand the player's hand
      * @return the hand string code
      */
-    private String createHandString(Hand hand){
+    private String createHandString(Hand hand) {
         // detect pair
-        if(hand.isPair()){
-            if(hand.getCard(0).isAce() || hand.getCard(0).value() == 8){
+        if (hand.isPair()) {
+            if (hand.getCard(0).isAce() || hand.getCard(0).value() == 8) {
                 return "A,A 8,8";
             }
-            if(hand.getCard(0).isFace()){
+            if (hand.getCard(0).isFace()) {
                 return "10, 10";
             }
             int v = hand.getCard(0).value();
             return v + ", " + v;
         }
         boolean containsAce = false;
-        for(int i = 0; i < hand.size(); i++){
-            if(hand.getCard(i).isAce()){
+        for (int i = 0; i < hand.size(); i++) {
+            if (hand.getCard(i).isAce()) {
                 containsAce = true;
                 break;
             }
         }
-        if(containsAce){
+        if (containsAce) {
             int acecount = 0;
             int value = 0;
-            for(int i = 0; i < hand.size(); i++){
-                if(hand.getCard(i).isAce()){
+            for (int i = 0; i < hand.size(); i++) {
+                if (hand.getCard(i).isAce()) {
                     acecount++;
-                }
-                else{
+                } else {
                     value += hand.getCard(i).value();
                 }
-                
+
             }
-            for(int i = 1; i < acecount; i++){
+            for (int i = 1; i < acecount; i++) {
                 value += 1;
             }
-            if(value <= 10){
-                if(value >= 8){
+            if (value <= 10) {
+                if (value >= 8) {
                     return "A, 8-10";
                 }
                 return "A, " + value;
             }
         }
         int value = 0;
-        for(int i = 0; i < hand.size(); i++){
+        for (int i = 0; i < hand.size(); i++) {
             value += hand.getCard(i).value();
         }
-        if(value >= 17){
+        if (value >= 17) {
             return "17+";
         }
-        if(value <= 8){
+        if (value <= 8) {
             return "5-8";
         }
         return "" + value;
--- src/charlie/bot/server/PerfectPlay.java
+++ src/charlie/bot/server/PerfectPlay.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package charlie.bot.server;
 
 import charlie.advisor.BasicStrategy;
@@ -20,17 +19,17 @@ import java.util.Random;
  *
  * @author Phil
  */
-public class PerfectPlay implements Runnable{
-    
+public class PerfectPlay implements Runnable {
+
     private IBot bot;
     private Hid hid;
     private Hand hand;
     private Dealer dealer;
     private Card upCard;
     private Random random;
-    
-    public PerfectPlay(IBot bot, Dealer dealer, Hid hid, Hand hand, 
-            Card upCard){
+
+    public PerfectPlay(IBot bot, Dealer dealer, Hid hid, Hand hand,
+            Card upCard) {
         this.bot = bot;
         this.hid = hid;
         this.hand = hand;
@@ -38,29 +37,29 @@ public class PerfectPlay implements Runnable{
         this.upCard = upCard;
         random = new Random(1);
     }
-    
-    public void setUpCard(Card upCard){
+
+    public void setUpCard(Card upCard) {
         this.upCard = upCard;
     }
-    
+
     @Override
-    public void run(){
-        
-            System.out.println("play recieved " + hand.size());
+    public void run() {
+
+        System.out.println("play recieved " + hand.size());
         for (int x = 0; x < hand.size(); x++) {
             System.out.println(hand.getCard(x).getName());
         }
         if (!(hand.isBroke() || hand.isBlackjack()
-                || hand.getValue() == 21)) {
+                || hand.getValue() == 21 || hand.isCharlie())) {
             try {
                 //Thread.
                 Thread.sleep(2500);
             } catch (InterruptedException ie) {
                 // log exception?
             }
-            Play play = new BasicStrategy().advise(this.hand, this.upCard);
+            Play play = StrategyCard.getStrategyCard().getBotPlay(this.hand, this.upCard);
 
-            if (play == Play.HIT || play == Play.SPLIT) {
+            if (play == Play.HIT) {
                 this.dealer.hit(this.bot, this.hid);
                 //this.dealer.hit(this, this.hid);
                 this.bot.play(this.hid);
