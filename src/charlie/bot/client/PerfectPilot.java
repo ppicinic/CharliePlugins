b609e12ea42b69d010d3b8a5ffdf4457f1575c5d
Gregory Cremins <coolwhar@gmail.com>
2014-04-29 23:54:46 -0400

    Implemented the graphics of UI

--- src/charlie/bot/client/PerfectPilot.java
+++ src/charlie/bot/client/PerfectPilot.java
@@ -7,7 +7,11 @@ import charlie.card.Hid;
 import charlie.dealer.Seat;
 import charlie.plugin.IGerty;
 import charlie.view.AMoneyManager;
+import java.awt.BasicStroke;
 import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.Font;
+import java.text.DecimalFormat;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,12 +26,32 @@ public class PerfectPilot implements IGerty{
     private static final int minBetAmt = 5;
     private Courier courier;
     private AMoneyManager moneyManager;
+    //shoe size
     private int shoeSize;
     private Hand hand;
     private Card upCard;
+    //num games
     private int gameCount;
+    //card count
     private int count;
     private int aceCount;
+    private int amt;
+    private int totalBet;
+    private int numBlackJacks;
+    private int numCharlies;
+    private int numWins;
+    private int numBreaks;
+    private int numLoses;
+    DecimalFormat df = new DecimalFormat("#.##");
+    protected Font font = new Font("Arial", Font.BOLD, 15);
+    protected Font font2 = new Font("Arial", Font.BOLD, 9);
+    protected float dash1[] = {10.0f};
+    protected BasicStroke dashed
+            = new BasicStroke(3.0f,
+                    BasicStroke.CAP_BUTT,
+                    BasicStroke.JOIN_MITER,
+                    10.0f, dash1, 0.0f);  
+    
     
     public PerfectPilot(){
         gameCount = 0;
@@ -37,7 +61,7 @@ public class PerfectPilot implements IGerty{
 
     @Override
     public void go() {
-        int amt = 0;
+        amt = 0;
         moneyManager.clearBet();
         if(count > 0){
             for(int i = 0; i < count; i++){
@@ -51,6 +75,7 @@ public class PerfectPilot implements IGerty{
         courier.bet(amt, 0);
         gameCount++;
         log.info("game number: " + gameCount);
+        totalBet += amt;
     }
 
     @Override
@@ -70,6 +95,40 @@ public class PerfectPilot implements IGerty{
 
     @Override
     public void render(Graphics2D g) {
+        
+        //Render counting system
+        g.setFont(font);
+        g.setColor(Color.BLUE);
+        g.drawString("Advanced Omega 2", 0, 255);
+        //Render rectangle container
+        g.setColor(Color.RED);
+        g.fillRect(0, 260, 200, 140);
+        g.setColor(Color.YELLOW);
+        g.setStroke(dashed);
+        g.drawRect(0, 260, 200, 140);
+        //Render the Shoe size
+        g.setColor(Color.orange);
+        g.drawString("Shoe count : " + df.format(shoeSize), 5, 280);
+        //Render the count
+        g.drawString("Count: " + count, 5, 300);
+        //Render the games played
+        g.drawString("Games Played : " + gameCount, 5, 320);
+        //Render the max bet
+        g.drawString("Max bet: N/A", 5, 340);
+        //Render the mean bet
+        if(gameCount == 0)
+        {
+            g.drawString("Mean Bet: 0", 5, 360);
+        }
+        else
+        {
+            g.drawString("Mean Bet: " + df.format(totalBet /(double) gameCount), 5, 360);
+        }
+        //Render the counts
+        g.setFont(font2);
+        g.drawString("BlackJacks|Charlies|Wins|Breaks|Loses", 5, 380);
+        g.setFont(font);
+        g.drawString(numBlackJacks + "|" + numCharlies + "|" + numWins + "|" + numBreaks + "|" + numLoses, 5, 395);
         
     }
 
@@ -123,31 +182,36 @@ public class PerfectPilot implements IGerty{
 
     @Override
     public void bust(Hid hid) {
-        
+        numLoses++;
     }
 
     @Override
     public void win(Hid hid) {
+        numWins++;
         
     }
 
     @Override
     public void blackjack(Hid hid) {
+        numBlackJacks++;
         
     }
 
     @Override
     public void charlie(Hid hid) {
+        numCharlies++;
        
     }
 
     @Override
     public void lose(Hid hid) {
+        numLoses++;
         
     }
 
     @Override
     public void push(Hid hid) {
+        numBreaks++;
         
     }
 
